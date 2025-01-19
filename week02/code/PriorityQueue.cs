d1c03ffec36f258c35c0d2276b98ca471b59a01f
wcvaughan <chrisvaughanlds@gmail.com>
2025-01-19 02:06:21 -0900

    forgot to do this part

--- week02/code/PriorityQueue.cs
+++ week02/code/PriorityQueue.cs
@@ -24,14 +24,18 @@
 
         // Find the index of the item with the highest priority to remove
         var highPriorityIndex = 0;
-        for (int index = 1; index < _queue.Count - 1; index++)
+        for (int index = 0; index < _queue.Count; index++)
         {
-            if (_queue[index].Priority >= _queue[highPriorityIndex].Priority)
+            if (_queue[index].Priority > _queue[highPriorityIndex].Priority)
                 highPriorityIndex = index;
         }
 
         // Remove and return the item with the highest priority
         var value = _queue[highPriorityIndex].Value;
+
+        // the item is removed from the list
+        _queue.RemoveAt(highPriorityIndex);
+
         return value;
     }
 
--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,23 +6,56 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
+    // Scenario: Enqueue adds an item to the back of _queue and contains both data and priority
     // Expected Result: 
     // Defect(s) Found: 
     public void TestPriorityQueue_1()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("highspren", 1);
+        Assert.IsNotNull(priorityQueue.Dequeue());
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Dequeue removes the item with highest priority and returns its value
+    // Expected Result: The value retrieved by the Dequeue method will be "tin"
+    // Defect(s) Found: tin expected but bronze was returned
     public void TestPriorityQueue_2()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("steel", 1);
+        priorityQueue.Enqueue("zinc", 2);
+        priorityQueue.Enqueue("bronze", 3);
+        priorityQueue.Enqueue("tin", 4);
+        Assert.AreEqual("tin", priorityQueue.Dequeue());
+
+    }
+
+    [TestMethod]
+    // Scenario: Two items share the same priority, Dequeue removes the item closest to the front of the queue and its value is returned
+    // Expected Result: firstItem value will be "threnody" while secondItem value will be "yolen"
+    // Defect(s) Found: "threnody" is the same value returned instead of "yolen"
+    public void TestPriorityQueue_3()
+    {
+        var priorityQueue = new PriorityQueue();
+        priorityQueue.Enqueue("roshar", 1);
+        priorityQueue.Enqueue("canticle", 2);
+        priorityQueue.Enqueue("threnody", 4);
+        priorityQueue.Enqueue("yolen", 4);
+        string firstItem = priorityQueue.Dequeue();
+        string secondItem = priorityQueue.Dequeue();
+        Assert.AreEqual("yolen", firstItem);
+        Assert.AreEqual("threnody", secondItem);
+    }
+
+    [TestMethod]
+    // Scenario: If the queue is empty an exception error is thrown when Dequeue is called
+    // Expected Result: Error is thrown
+    // Defect(s) Found: none
+    public void TestPriorityQueue_4()
+    {
+        var priorityQueue = new PriorityQueue();
+        Assert.ThrowsException<InvalidOperationException>(() => priorityQueue.Dequeue());
     }
 
     // Add more test cases as needed below.
